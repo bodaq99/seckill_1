7b9eec5769a8559af23ea9799101ca72a0e8ade3
阿齐 <2608404777@qq.com>
2025-10-08 19:57:49 +0800

    345

--- seckill_aq/src/main/java/org/example/seckill_aq/helloworld.java
+++ seckill_aq/src/main/java/org/example/seckill_aq/helloworld.java
@@ -6,6 +6,6 @@ public class helloworld {
         System.out.println("hello world");
         System.out.println("hello world");
         System.out.println("hello world");
-        System.out.println("hello world");
+        System.out.println("345");
     }
 }
