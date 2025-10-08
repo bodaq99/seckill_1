624b52741da08da9b557fbf4d5ff15e8f3c33a04
阿齐 <2608404777@qq.com>
2025-10-08 19:50:19 +0800

    合并seckill_fen

--- seckill_aq/src/main/java/org/example/seckill_aq/helloworld.java
+++ seckill_aq/src/main/java/org/example/seckill_aq/helloworld.java
@@ -3,5 +3,9 @@ package org.example.seckill_aq;
 public class helloworld {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
     }
 }
