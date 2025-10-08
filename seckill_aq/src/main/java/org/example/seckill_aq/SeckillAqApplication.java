f01d06b85cf00bd578fe14d1449df6119d29e735
阿齐 <2608404777@qq.com>
2025-10-08 19:27:16 +0800

    1

---
+++ seckill_aq/src/main/java/org/example/seckill_aq/SeckillAqApplication.java
@@ -0,0 +1,13 @@
+package org.example.seckill_aq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeckillAqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeckillAqApplication.class, args);
+    }
+
+}
---
+++ seckill_aq/src/test/java/org/example/seckill_aq/SeckillAqApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.seckill_aq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeckillAqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
